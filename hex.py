70513952f5fe786cdc118cc3391ffb52595f44e8
Gordon Wrigley <gordon.wrigley@gmail.com>
2015-01-08 21:11:38 +0000

    factor out contexts add loop puzzle

--- hex.py
+++ hex.py
@@ -5,183 +5,239 @@ from itertools import chain
 from collections import defaultdict
 import sys
 import json
+from contextlib import contextmanager
 
-rows = [
-    r".*H.*H.*",
-    r"(DI|NS|TH|OM)*",
-    r"F.*[AO].*[AO].*",
-    r"(O|RHH|MM)*",
-    r".*",
-    r"C*MC(CCC|MM)*",
-    r"[^C]*[^R]*III.*",
-    r"(...?)\1*",
-    r"([^X]|XCC)*",
-    r"(RR|HHH)*.?",
-    r"N.*X.X.X.*E",
-    r"R*D*M*",
-    r".(C|HH)*",
-]
-cols = [
-    r"(ND|ET|IN)[^X]*",
-    r"[CHMNOR]*I[CHMNOR]*",
-    r"P+(..)\1.*",
-    r"(E|CR|MN)*",
-    r"([^MC]|MM|CC)*",
-    r"[AM]*CM(RC)*R?",
-    r".*",
-    r".*PRR.*DDC.*",
-    r"(HHX|[^HX])*",
-    r"([^EMC]|EM)*",
-    r".*OXR.*",
-    r".*LR.*RL.*",
-    r".*SE.*UE.*",
-]
-diags = [
-    r".*G.*V.*H.*",
-    r"[CR]*",
-    r".*XEXM*",
-    r".*DD.*CCM.*",
-    r".*XHCR.*X.*",
-    r".*(.)(.)(.)(.)\4\3\2\1.*",
-    r".*(IN|SE|HI)",
-    r"[^C]*MMM[^C]*",
-    r".*(.)C\1X\1.*",
-    r"[CEIMU]*OH[AEMOR]*",
-    r"(RX|[^R])*",
-    r"[^M]*M[^M]*",
-    r"(S|MM|HHH)*",
-]
-edges = [rows, cols, diags]
-
-answer = {
-    (0, 0): "N",
-    (0, 1): "D",
-    (0, 2): "F",
-    (0, 3): "M",
-    (0, 4): "M",
-    (0, 5): "C",
-    (0, 6): "H",
-    (1, 0): "H",
-    (1, 1): "I",
-    (1, 2): "O",
-    (1, 3): "M",
-    (1, 4): "C",
-    (1, 5): "M",
-    (1, 6): "R",
-    (1, 7): "O",
-    (2, 0): "P",
-    (2, 1): "O",
-    (2, 2): "X",
-    (2, 3): "O",
-    (2, 4): "X",
-    (2, 5): "C",
-    (2, 6): "X",
-    (2, 7): "R",
-    (2, 8): "V",
-    (3, 0): "E",
-    (3, 1): "M",
-    (3, 2): "N",
-    (3, 3): "M",
-    (3, 4): "N",
-    (3, 5): "C",
-    (3, 6): "R",
-    (3, 7): "E",
-    (3, 8): "C",
-    (3, 9): "R",
-    (4, 0): "H",
-    (4, 1): "O",
-    (4, 2): "X",
-    (4, 3): "M",
-    (4, 4): "M",
-    (4, 5): "C",
-    (4, 6): "C",
-    (4, 7): "O",
-    (4, 8): "X",
-    (4, 9): "R",
-    (4, 10): "N",
-    (5, 0): "A",
-    (5, 1): "M",
-    (5, 2): "A",
-    (5, 3): "M",
-    (5, 4): "M",
-    (5, 5): "C",
-    (5, 6): "M",
-    (5, 7): "R",
-    (5, 8): "C",
-    (5, 9): "R",
-    (5, 10): "C",
-    (5, 11): "R",
-    (6, 0): "S",
-    (6, 1): "T",
-    (6, 2): "X",
-    (6, 3): "M",
-    (6, 4): "C",
-    (6, 5): "M",
-    (6, 6): "I",
-    (6, 7): "E",
-    (6, 8): "C",
-    (6, 9): "R",
-    (6, 10): "X",
-    (6, 11): "R",
-    (6, 12): "G",
-    (7, 1): "H",
-    (7, 2): "P",
-    (7, 3): "R",
-    (7, 4): "R",
-    (7, 5): "M",
-    (7, 6): "I",
-    (7, 7): "O",
-    (7, 8): "H",
-    (7, 9): "H",
-    (7, 10): "D",
-    (7, 11): "D",
-    (7, 12): "C",
-    (8, 2): "H",
-    (8, 3): "H",
-    (8, 4): "X",
-    (8, 5): "M",
-    (8, 6): "I",
-    (8, 7): "R",
-    (8, 8): "H",
-    (8, 9): "H",
-    (8, 10): "X",
-    (8, 11): "D",
-    (8, 12): "C",
-    (9, 3): "H",
-    (9, 4): "E",
-    (9, 5): "M",
-    (9, 6): "H",
-    (9, 7): "E",
-    (9, 8): "M",
-    (9, 9): "H",
-    (9, 10): "E",
-    (9, 11): "M",
-    (9, 12): "H",
-    (10, 4): "M",
-    (10, 5): "M",
-    (10, 6): "X",
-    (10, 7): "O",
-    (10, 8): "X",
-    (10, 9): "R",
-    (10, 10): "X",
-    (10, 11): "M",
-    (10, 12): "H",
-    (11, 5): "M",
-    (11, 6): "L",
-    (11, 7): "R",
-    (11, 8): "C",
-    (11, 9): "R",
-    (11, 10): "L",
-    (11, 11): "M",
-    (11, 12): "C",
-    (12, 6): "S",
-    (12, 7): "E",
-    (12, 8): "C",
-    (12, 9): "U",
-    (12, 10): "E",
-    (12, 11): "M",
-    (12, 12): "C",
+original_puzzle = {
+    "rows": [
+        r".*H.*H.*",
+        r"(DI|NS|TH|OM)*",
+        r"F.*[AO].*[AO].*",
+        r"(O|RHH|MM)*",
+        r".*",
+        r"C*MC(CCC|MM)*",
+        r"[^C]*[^R]*III.*",
+        r"(...?)\1*",
+        r"([^X]|XCC)*",
+        r"(RR|HHH)*.?",
+        r"N.*X.X.X.*E",
+        r"R*D*M*",
+        r".(C|HH)*",
+    ],
+    "cols": [
+        r"(ND|ET|IN)[^X]*",
+        r"[CHMNOR]*I[CHMNOR]*",
+        r"P+(..)\1.*",
+        r"(E|CR|MN)*",
+        r"([^MC]|MM|CC)*",
+        r"[AM]*CM(RC)*R?",
+        r".*",
+        r".*PRR.*DDC.*",
+        r"(HHX|[^HX])*",
+        r"([^EMC]|EM)*",
+        r".*OXR.*",
+        r".*LR.*RL.*",
+        r".*SE.*UE.*",
+    ],
+    "diags": [
+        r".*G.*V.*H.*",
+        r"[CR]*",
+        r".*XEXM*",
+        r".*DD.*CCM.*",
+        r".*XHCR.*X.*",
+        r".*(.)(.)(.)(.)\4\3\2\1.*",
+        r".*(IN|SE|HI)",
+        r"[^C]*MMM[^C]*",
+        r".*(.)C\1X\1.*",
+        r"[CEIMU]*OH[AEMOR]*",
+        r"(RX|[^R])*",
+        r"[^M]*M[^M]*",
+        r"(S|MM|HHH)*",
+    ],
+    "answer": {
+        (0, 0): "N",
+        (0, 1): "D",
+        (0, 2): "F",
+        (0, 3): "M",
+        (0, 4): "M",
+        (0, 5): "C",
+        (0, 6): "H",
+        (1, 0): "H",
+        (1, 1): "I",
+        (1, 2): "O",
+        (1, 3): "M",
+        (1, 4): "C",
+        (1, 5): "M",
+        (1, 6): "R",
+        (1, 7): "O",
+        (2, 0): "P",
+        (2, 1): "O",
+        (2, 2): "X",
+        (2, 3): "O",
+        (2, 4): "X",
+        (2, 5): "C",
+        (2, 6): "X",
+        (2, 7): "R",
+        (2, 8): "V",
+        (3, 0): "E",
+        (3, 1): "M",
+        (3, 2): "N",
+        (3, 3): "M",
+        (3, 4): "N",
+        (3, 5): "C",
+        (3, 6): "R",
+        (3, 7): "E",
+        (3, 8): "C",
+        (3, 9): "R",
+        (4, 0): "H",
+        (4, 1): "O",
+        (4, 2): "X",
+        (4, 3): "M",
+        (4, 4): "M",
+        (4, 5): "C",
+        (4, 6): "C",
+        (4, 7): "O",
+        (4, 8): "X",
+        (4, 9): "R",
+        (4, 10): "N",
+        (5, 0): "A",
+        (5, 1): "M",
+        (5, 2): "A",
+        (5, 3): "M",
+        (5, 4): "M",
+        (5, 5): "C",
+        (5, 6): "M",
+        (5, 7): "R",
+        (5, 8): "C",
+        (5, 9): "R",
+        (5, 10): "C",
+        (5, 11): "R",
+        (6, 0): "S",
+        (6, 1): "T",
+        (6, 2): "X",
+        (6, 3): "M",
+        (6, 4): "C",
+        (6, 5): "M",
+        (6, 6): "I",
+        (6, 7): "E",
+        (6, 8): "C",
+        (6, 9): "R",
+        (6, 10): "X",
+        (6, 11): "R",
+        (6, 12): "G",
+        (7, 1): "H",
+        (7, 2): "P",
+        (7, 3): "R",
+        (7, 4): "R",
+        (7, 5): "M",
+        (7, 6): "I",
+        (7, 7): "O",
+        (7, 8): "H",
+        (7, 9): "H",
+        (7, 10): "D",
+        (7, 11): "D",
+        (7, 12): "C",
+        (8, 2): "H",
+        (8, 3): "H",
+        (8, 4): "X",
+        (8, 5): "M",
+        (8, 6): "I",
+        (8, 7): "R",
+        (8, 8): "H",
+        (8, 9): "H",
+        (8, 10): "X",
+        (8, 11): "D",
+        (8, 12): "C",
+        (9, 3): "H",
+        (9, 4): "E",
+        (9, 5): "M",
+        (9, 6): "H",
+        (9, 7): "E",
+        (9, 8): "M",
+        (9, 9): "H",
+        (9, 10): "E",
+        (9, 11): "M",
+        (9, 12): "H",
+        (10, 4): "M",
+        (10, 5): "M",
+        (10, 6): "X",
+        (10, 7): "O",
+        (10, 8): "X",
+        (10, 9): "R",
+        (10, 10): "X",
+        (10, 11): "M",
+        (10, 12): "H",
+        (11, 5): "M",
+        (11, 6): "L",
+        (11, 7): "R",
+        (11, 8): "C",
+        (11, 9): "R",
+        (11, 10): "L",
+        (11, 11): "M",
+        (11, 12): "C",
+        (12, 6): "S",
+        (12, 7): "E",
+        (12, 8): "C",
+        (12, 9): "U",
+        (12, 10): "E",
+        (12, 11): "M",
+        (12, 12): "C",
+    },
 }
+
+loop_puzzle = {
+    "rows": [
+        r".X",
+        r"(.)\1\1",
+        r"XX",
+    ],
+    "cols": [
+        r"BA|AC|BB",
+        r".*",
+        r".*",
+    ],
+    "diags": [
+        r".*",
+        r".(CB|AA|AB)",
+        r".*",
+    ],
+    "answer": {
+        (0, 0): "B",
+        (0, 1): "B",
+        (1, 0): "X",
+        (1, 1): "B",
+        (1, 2): "X",
+        (2, 1): "B",
+        (2, 2): "X",
+    },
+}
+
+context_puzzle = {
+    "rows": [
+        r"XX",
+        r"(.)\1\1",
+        r"XX",
+    ],
+    "cols": [
+        r"X[ABC]",
+        r"X[AB]X",
+        r"[AC]X",
+    ],
+    "diags": [
+        r".*",
+        r".*",
+        r".*",
+    ],
+    "answer": {
+        (0, 0): "X",
+        (0, 1): "A",
+        (1, 0): "X",
+        (1, 1): "A",
+        (1, 2): "X",
+        (2, 1): "A",
+        (2, 2): "X",
+    },
+}
+
 
 def get_char(e, chars):
     if e and e[0] in chars:
@@ -194,6 +250,38 @@ LETTERS = string.ascii_uppercase
 DIGITS = string.digits
 
 
+class State(object):
+    def __init__(self, remaining, used=(), context=()):
+        self.remaining = tuple(remaining)
+        self.used = tuple(used)
+        self.context = tuple(context)
+
+    def consume(self, chars):
+        if self.remaining:
+            hits = self.remaining[0] & chars
+            if hits:
+                return State(self.remaining[1:], self.used + (hits,), self.context)
+        return None
+
+    def create_context(self):
+        context = self.context + (None,)
+        return State(self.remaining, self.used, context)
+
+    def set_context(self, old_state):
+        # old_state must be the state returned by create_context
+        context = list(self.context)
+        context[len(old_state.context) - 1] = (len(old_state.used), len(self.used))
+        return State(self.remaining, self.used, context)
+
+    def consume_context(self, index):
+        s,e = self.context[index]
+        consume = tuple(o & n for o, n in zip(self.used[s:e], self.remaining))
+        if all(consume) and len(consume) == e-s:
+            remaining = self.remaining[len(consume):]
+            used = self.used[:s] + consume + self.used[e:] + consume
+            return State(remaining, used, self.context)
+
+
 class OrChain(object):
     def __init__(self, e):
         self.options = [ExpressionList(e)]
@@ -203,17 +291,12 @@ class OrChain(object):
     def __str__(self):
         return "|".join(str(o) for o in self.options)
 
-    def match(self, used, remaining, context):
-        index = len(context)
-        context.append(None)
+    def match(self, state):
+        state = state.create_context()
 
         for option in self.options:
-            for new_used, new_context in option.match(used, remaining, context):
-                context[index] = len(used), len(new_used)
-                yield new_used, new_context
-
-        assert len(context) == index + 1
-        context.pop()
+            for new_state in option.match(state):
+                yield new_state.set_context(state)
 
 
 class ExpressionList(object):
@@ -225,18 +308,17 @@ class ExpressionList(object):
     def __str__(self):
         return "".join(str(o) for o in self.exps)
 
-    def match(self, used, remaining, context):
-        def recurse(used, remaining, context, exps):
+    def match(self, state):
+        def recurse(state, exps):
             if exps:
-                for partial_used, partial_context in exps[0].match(used, remaining, context):
-                    partial_remaining = remaining[len(partial_used)-len(used):]
-                    for new_used, new_context in recurse(partial_used, partial_remaining, partial_context, exps[1:]):
-                        yield new_used, new_context
+                for partial_state in exps[0].match(state):
+                    for new_state in recurse(partial_state, exps[1:]):
+                        yield new_state
             else:
-                yield used, context
+                yield state
 
-        for new_used, new_context in recurse(used, remaining, context, self.exps):
-            yield new_used, new_context
+        for new_state in recurse(state, self.exps):
+            yield new_state
 
 
 class ModifierExpression(object):
@@ -255,24 +337,23 @@ class ModifierExpression(object):
             res += self.modifier
         return res
 
-    def match(self, used, remaining, context):
-        def recurse(used, remaining, context):
-            for partial_used, partial_context in self.expression.match(used, remaining, context):
-                if len(partial_used) > len(used):
-                    yield partial_used, partial_context
-                    partial_remaining = remaining[len(partial_used)-len(used):]
-                    for new_used, new_context in recurse(partial_used, partial_remaining, partial_context):
-                        yield new_used, new_context
+    def match(self, state):
+        def recurse(state):
+            for partial_state in self.expression.match(state):
+                if len(partial_state.used) > len(state.used):
+                    yield partial_state
+                    for new_state in recurse(partial_state):
+                        yield new_state
 
         if self.modifier in list("?*"):
-            yield used, context
+            yield state
 
         if self.modifier in list("+*"):
-            for new_used, new_context in recurse(used, remaining, context):
-                yield new_used, new_context
+            for new_state in recurse(state):
+                yield new_state
         else:
-            for new_used, new_context in self.expression.match(used, remaining, context):
-                yield new_used, new_context
+            for new_state in self.expression.match(state):
+                yield new_state
 
 
 def BasicExpression(e):
@@ -291,7 +372,6 @@ def BasicExpression(e):
         res = OrChain(e)
         assert get_char(e, ")")
     elif get_char(e, "\\"):
-        # hack around \n expressions for now by treating them as .*
         res = ClassExpression(e)
     elif get_char(e, "."):
         res = Letters(LETTERS)
@@ -310,11 +390,10 @@ class ClassExpression(object):
     def __str__(self):
         return "\\" + str(self.group)
 
-    def match(self, used, remaining, context):
-        s,e = context[self.group]
-        consume = [o & n for o, n in zip(used[s:e], remaining)]
-        if all(consume) and len(consume) == e-s:
-            yield used[:s] + consume + used[e:] + consume, context
+    def match(self, state):
+        new_state = state.consume_context(self.group)
+        if new_state:
+            yield new_state
 
 
 class Letters(object):
@@ -331,11 +410,10 @@ class Letters(object):
         else:
             return "[" + "".join(sorted(self.options)) + "]"
 
-    def match(self, used, remaining, context):
-        if remaining:
-            hits = remaining[0] & self.options
-            if hits:
-                yield used + [hits], context
+    def match(self, state):
+        new_state = state.consume(self.options)
+        if new_state:
+            yield new_state
 
 
 def parse(regexp_str):
@@ -347,9 +425,9 @@ def parse(regexp_str):
 
 def match(regexp, char_list):
     new_char_lists = []
-    for new_char_list, new_context in regexp.match([], char_list, []):
-        if len(new_char_list) == len(char_list):
-            new_char_lists.append(new_char_list)
+    for new_state in regexp.match(State(char_list)):
+        if not new_state.remaining:
+            new_char_lists.append(new_state.used)
     assert new_char_lists
     new_char_list = [set(chain(*x)) for x in zip(*new_char_lists)]
     return new_char_list
@@ -434,12 +512,16 @@ class HexGrid(object):
             sys.stdout.write("\n")
 
 
-def solve(edges):
+def flat(cells):
+    return ["".join(sorted(c)) for c in cells]
+
+
+def solve(puzzle):
     # check the grid is symettric
     grid = HexGrid(
-        [parse(e) for e in rows],
-        [parse(e) for e in cols],
-        [parse(e) for e in diags],
+        [parse(e) for e in puzzle["rows"]],
+        [parse(e) for e in puzzle["cols"]],
+        [parse(e) for e in puzzle["diags"]],
     )
 
     queue = set(range(len(grid.lines)))
@@ -448,15 +530,17 @@ def solve(edges):
         line_id = queue.pop()
         regex, cell_ids = grid.lines[line_id]
         cells = [grid.cells[cell_id] for cell_id in cell_ids]
+#        print line_id, regex
+#        print flat(cells)
         new_cells = match(regex, cells)
+#        print flat(new_cells)
+#        print
         for i, o, n in zip(cell_ids, cells, new_cells):
             if n != o:
                 grid.cells[i] = n
                 queue.update(grid.cell_lines[i])
-                queue.remove(line_id)
     grid.dump()
-
-    assert {k: "".join(v) for k, v in grid.cells.iteritems()} == answer
+    assert {k: "".join(v) for k, v in grid.cells.iteritems()} == puzzle["answer"]
 
 
 if __name__ == "__main__":
@@ -473,6 +557,6 @@ if __name__ == "__main__":
     assert match(parse("(.*)*"), c) == [set("BC"), set("AC"), set("AB")]
     assert match(parse(".*.*"), c) == [set("BC"), set("AC"), set("AB")]
 
-    solve(edges)
-
-
+    solve(original_puzzle)
+    solve(context_puzzle)
+#    solve(loop_puzzle)
